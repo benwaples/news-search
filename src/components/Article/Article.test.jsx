638adc4dd87e23c984cdadf21984c5ac5b3cb0f2
bwapes <benwaples@gmail.com>
2020-10-21 17:09:13 -0700

    article and article list

---
+++ src/components/Article/Article.test.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { 
+  cleanup, 
+  fireEvent, 
+  render, 
+  screen, 
+  waitFor 
+} from '@testing-library/react';
+import Article from './Article';
+
+describe('Article component testing', () => {
+  afterEach(() => cleanup());
+  it('renders one article', () => {
+    const article = {
+      source: 'article source',
+      title: 'test title',
+      author: 'fake af author',
+      content: 'some true stuff',
+      imageUrl: 'https://fakeplaceholder.image.url.you/couldnt',
+      url: 'google.com',
+  
+    };
+    render(<Article article={article} />);
+
+    screen.getByText('test title');
+  });
+});
+
+
---
+++ src/components/ArticleList/ArticleList.test.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { 
+  cleanup, 
+  fireEvent, 
+  render, 
+  screen, 
+  waitFor 
+} from '@testing-library/react';
+import ArticleList from './ArticleList';
+
+describe('test the ArticleList component', () => {
+  afterEach(() => cleanup());
+  it('should render a list of articles', () => {
+    const articles = [
+      {
+        source: 'article source',
+        title: 'test title3',
+        author: 'fake af author',
+        content: 'some true stuff',
+        imageUrl: 'https://fakeplaceholder.image.url.you/couldnt',
+        url: 'google.com',
+      },
+      {
+        source: 'article source',
+        title: 'test title2',
+        author: 'fake af author',
+        content: 'some true stuff',
+        imageUrl: 'https://fakeplaceholder.image.url.you/couldnt',
+        url: 'google.com',
+      },
+      {
+        source: 'article source',
+        title: 'test title1',
+        author: 'fake af author',
+        content: 'some true stuff',
+        imageUrl: 'https://fakeplaceholder.image.url.you/couldnt',
+        url: 'google.com',
+      }
+    ];
+
+    render(<ArticleList articles={articles} />);
+
+    const allArticleTitles = screen.getAllByText('some true stuff');
+
+    expect(allArticleTitles.length).toEqual(3);
+  });
+});
